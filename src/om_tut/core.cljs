9b886cf18d2052421b856d656cf60498e705d928
FrankApiyo <franklineapiyo@gmail.com>
2020-02-24 21:34:25 +0300

    First om component

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -1,6 +1,6 @@
 (ns om-tut.core
-    (:require [om.core :as om :include-macros true]
-              [om.dom :as dom :include-macros true]))
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]))
 
 (enable-console-print!)
 
@@ -10,23 +10,41 @@
 
 (defonce app-state
   (atom
-   {:list ["Lion" "Zebra" "Buffalo" "Antelope"]}))
+   {:contacts [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+               {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
+               {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+               {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+               {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
+               {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]}))
+(defn middle-name [{:keys [middle middle-initial]}]
+  (cond
+    middle (str " " middle)
+    middle-initial (str " " middle-initial ".")))
+
+(defn display-name [{:keys [first last] :as contact}]
+  (str last ", " first (middle-name contact)))
 
 
-(defn stripe [text bgc]
-  (let [st #js {:backgroundColor bgc}]
-    (dom/li #js {:style st} text)))
+(defn contact-view [contact owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/li nil (display-name contact)))))
 
-(om/root
- (fn [data owner]
-   (om/component
-    (apply dom/ul #js {:className "animal"}
-           (map stripe (:list data) (cycle ["#ff0" "#fff"])))))
- app-state
- {:target (. js/document (getElementById "app0"))})
+(defn contacts-view [data owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div nil
+               (dom/h2 nil "Contact list")
+               (apply  dom/ul nil
+                       (om/build-all contact-view (:contacts data)))))))
+
+(om/root contacts-view app-state
+         {:target (. js/document (getElementById "contacts"))})
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  )
