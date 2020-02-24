33f1440513e9238a33da2da00077a330250cbc06
FrankApiyo <franklineapiyo@gmail.com>
2020-02-24 21:23:57 +0300

    Rendering a list of things with om

---
+++ src/om_tut/core.cljs
@@ -0,0 +1,32 @@
+(ns om-tut.core
+    (:require [om.core :as om :include-macros true]
+              [om.dom :as dom :include-macros true]))
+
+(enable-console-print!)
+
+(println "This text is printed from src/om-tut/core.cljs. Go ahead and edit it and see reloading in action.")
+
+;; define your app data so that it doesn't get over-written on reload
+
+(defonce app-state
+  (atom
+   {:list ["Lion" "Zebra" "Buffalo" "Antelope"]}))
+
+
+(defn stripe [text bgc]
+  (let [st #js {:backgroundColor bgc}]
+    (dom/li #js {:style st} text)))
+
+(om/root
+ (fn [data owner]
+   (om/component
+    (apply dom/ul #js {:className "animal"}
+           (map stripe (:list data) (cycle ["#ff0" "#fff"])))))
+ app-state
+ {:target (. js/document (getElementById "app0"))})
+
+(defn on-js-reload []
+  ;; optionally touch your app-state to force rerendering depending on
+  ;; your application
+  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+)
